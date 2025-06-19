69242af9359ec77998df06ec809c1cc0248cd11f
BenoitBenM <benoitbenm@gmail.com>
2025-06-19 05:51:52 +0200

    NEw attempt

--- Robot_Functions/ROS2/gbot_ws/src/mybot_nav/mybot_nav/mqtt_serial_bridge.py
+++ Robot_Functions/ROS2/gbot_ws/src/mybot_nav/mybot_nav/mqtt_serial_bridge.py
@@ -234,6 +234,7 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("robot/manual/command")
     client.subscribe("robot/manual/command")
     client.subscribe("robot/goto_keyloc")
+    client.subscribe("robot/auto/key/assign")
     client.publish("robot/auto/key/locations", json.dumps(key_locations))
 
 #     # Removed the thread for continuous CV and put it in follow mode
